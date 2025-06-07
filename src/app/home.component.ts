e6813d48e9505f1634dac59730e0a0987576c37d
KSkryabin <k.skryabin@mobidev.biz>
2025-06-07 21:14:49 +0300

    s

--- src/app/home.component.ts
+++ src/app/home.component.ts
@@ -1,22 +1,32 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { TableModule } from 'primeng/table';
+import { ProgressSpinnerModule } from 'primeng/progressspinner';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [TableModule],
+  imports: [TableModule, ProgressSpinnerModule, NgIf],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
   users: any[] = [];
+  loading = true;
 
   constructor(private http: HttpClient) {}
 
   ngOnInit() {
-    this.http
-      .get<any[]>('https://alseids-be.onrender.com/users')
-      .subscribe((users) => (this.users = users));
+    this.loading = true;
+    this.http.get<any[]>('https://alseids-be.onrender.com/users').subscribe({
+      next: (users) => {
+        this.users = users;
+        this.loading = false;
+      },
+      error: () => {
+        this.loading = false;
+      },
+    });
   }
 }
