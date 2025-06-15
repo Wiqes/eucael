92bd46036660a6e873adf66699e2e464796bc552
KSkryabin <k.skryabin@mobidev.biz>
2025-06-15 09:11:03 +0300

    refactor: wrap header in ng-container for conditional rendering on login page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,25 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, Router } from '@angular/router';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FormsModule, HttpClientModule],
+  imports: [RouterOutlet, FormsModule, HttpClientModule, NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'Alseids';
+  isLoginPage = false;
+
+  constructor(private router: Router) {
+    this.router.events.subscribe(() => {
+      this.isLoginPage = this.router.url === '/' || this.router.url.startsWith('/?');
+    });
+    // Set initial value
+    this.isLoginPage = this.router.url === '/' || this.router.url.startsWith('/?');
+  }
 }
