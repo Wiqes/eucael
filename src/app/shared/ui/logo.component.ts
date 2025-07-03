a5b3d755e9b804e556e55d7524a0dac6b54f7dc7
KSkryabin <k.skryabin@mobidev.biz>
2025-07-03 18:05:26 +0300

    style: adjust header padding and user avatar component for improved layout consistency

--- src/app/shared/ui/logo.component.ts
+++ src/app/shared/ui/logo.component.ts
@@ -12,6 +12,11 @@ import { Component } from '@angular/core';
         display: flex;
         justify-content: center;
         align-items: center;
+        background: #3f3f46;
+        box-shadow: 0 8px 32px rgba(0, 0, 0, 0.3);
+        border: 1px solid #dfe7ef;
+        border-radius: 5px;
+        height: 40px;
       }
 
       img {
