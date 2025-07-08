c3c60fb5a373b5c10915cbf40c5eef283eeed3a7
KSkryabin <k.skryabin@mobidev.biz>
2025-07-08 15:10:52 +0300

    feat: implement internationalization for messages and update message service to use translations

--- src/app/core/services/message.service.ts
+++ src/app/core/services/message.service.ts
@@ -1,14 +1,22 @@
 import { inject, Injectable } from '@angular/core';
 import { IMessage } from '../models/message.model';
 import { MessageService as PrimeNGMessageService } from 'primeng/api';
+import { TranslateService } from '@ngx-translate/core';
 
 @Injectable({
   providedIn: 'root',
 })
 export class MessageService {
   private messageService = inject(PrimeNGMessageService);
+  private translate = inject(TranslateService);
 
   sendMessage(message: IMessage) {
-    this.messageService.add({ ...message, life: message.life ?? 3000 });
+    const translatedMessage = {
+      ...message,
+      summary: this.translate.instant(message.summary),
+      detail: this.translate.instant(message.detail),
+      life: message.life ?? 3000,
+    };
+    this.messageService.add(translatedMessage);
   }
 }
