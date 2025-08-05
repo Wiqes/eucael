06b96f406dd385982b23ee63cd5bc88c348d5523
KSkryabin <k.skryabin@mobidev.biz>
2025-08-05 09:21:11 +0300

    feat(bear-icon): add bear icon component with SVG and styles

---
+++ src/app/shared/ui/icons/bear-icon/bear-icon.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bear-icon',
+  imports: [],
+  templateUrl: './bear-icon.component.html',
+  styleUrl: './bear-icon.component.scss'
+})
+export class BearIconComponent {
+
+}
