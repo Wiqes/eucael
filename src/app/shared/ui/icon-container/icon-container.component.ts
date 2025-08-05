8af670a27ef39c9fe7cf598bace1ad7f491cb9a6
KSkryabin <k.skryabin@mobidev.biz>
2025-08-05 09:50:58 +0300

    feat(icon-container): add fox icon support and update icon component logic

--- src/app/shared/ui/icon-container/icon-container.component.ts
+++ src/app/shared/ui/icon-container/icon-container.component.ts
@@ -1,15 +1,26 @@
 import { Component, computed, input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BearIconComponent } from '../icons/bear-icon/bear-icon.component';
+import { FoxIconComponent } from '../icons/fox-icon/fox-icon.component';
 
 @Component({
   selector: 'app-icon-container',
-  imports: [CommonModule, BearIconComponent],
+  imports: [CommonModule, BearIconComponent, FoxIconComponent],
   templateUrl: './icon-container.component.html',
   styleUrls: ['./icon-container.component.scss'],
 })
 export class IconContainerComponent {
   color = input<string>('green');
+  iconName = input<string>('bear'); // Default icon
+  iconComponent = computed(() => {
+    switch (this.iconName()) {
+      case 'fox':
+        return FoxIconComponent;
+      case 'bear':
+      default:
+        return BearIconComponent;
+    }
+  });
 
   containerStyle = computed(() => ({
     display: 'flex',
---
+++ src/app/shared/ui/icons/fox-icon/fox-icon.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-fox-icon',
+  imports: [],
+  templateUrl: './fox-icon.component.html',
+  styleUrl: './fox-icon.component.scss'
+})
+export class FoxIconComponent {
+
+}
