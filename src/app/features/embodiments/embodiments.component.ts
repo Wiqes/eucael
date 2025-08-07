3ee6ca32caa1220df5abd9ea481f31888b9700e5
KSkryabin <k.skryabin@mobidev.biz>
2025-08-07 18:51:14 +0300

    feat(embodiments): update animal name handling and translation in multiselect component

--- src/app/features/embodiments/embodiments.component.ts
+++ src/app/features/embodiments/embodiments.component.ts
@@ -1,4 +1,5 @@
 import { Component, computed, effect, inject, signal } from '@angular/core';
+import { toSignal } from '@angular/core/rxjs-interop';
 import { StateService } from '../../core/services/state.service';
 import { NgFor } from '@angular/common';
 import { HomeComponent } from '../home/home.component';
@@ -11,7 +12,7 @@ import { LoaderComponent } from '../../shared/ui/loader/loader.component';
 import { AnimalComponent } from './animal/animal.component';
 import { IAnimal } from '../../core/models/entities/animal.model';
 import { IconService } from '../../core/services/icon.service';
-import { TranslateModule } from '@ngx-translate/core';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-embodiments',
@@ -31,8 +32,21 @@ import { TranslateModule } from '@ngx-translate/core';
 })
 export class EmbodimentsComponent extends HomeComponent {
   private readonly stateService = inject(StateService);
+  private translate = inject(TranslateService);
+  private newLanguageSignal = toSignal(this.translate.onLangChange.asObservable());
   protected iconService = inject(IconService);
-  animals = computed(() => this.stateService.animals());
+  animals = computed(() => {
+    const newLanguage = this.newLanguageSignal();
+    return this.stateService.animals().map((animal) => {
+      const animalName = animal.name
+        ? animal.name.charAt(0).toUpperCase() + animal.name.slice(1)
+        : '';
+      return {
+        ...animal,
+        name: this.translate.instant(animalName),
+      } as IAnimal;
+    });
+  });
   isTotemShown = false;
   isDataLoading = computed(() => this.stateService.isDataLoading());
   selectedAnimals = signal<IAnimal[]>([]);
