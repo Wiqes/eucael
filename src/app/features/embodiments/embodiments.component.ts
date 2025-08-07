8bca352ac0560ca4826ade84bcacb3b365c5acb0
KSkryabin <k.skryabin@mobidev.biz>
2025-08-07 14:19:08 +0300

    feat(embodiments): enhance animal selection with multiselect component and improve filtering logic

--- src/app/features/embodiments/embodiments.component.ts
+++ src/app/features/embodiments/embodiments.component.ts
@@ -1,16 +1,26 @@
-import { Component, computed, inject, signal } from '@angular/core';
+import { Component, computed, effect, inject, signal } from '@angular/core';
 import { StateService } from '../../core/services/state.service';
 import { NgFor } from '@angular/common';
 import { HomeComponent } from '../home/home.component';
 import { ToggleSwitch } from 'primeng/toggleswitch';
 import { FormsModule } from '@angular/forms';
+import { MultiSelectModule } from 'primeng/multiselect';
 import { NgClass } from '@angular/common';
 import { LoaderComponent } from '../../shared/ui/loader/loader.component';
 import { AnimalComponent } from './animal/animal.component';
+import { IAnimal } from '../../core/models/entities/animal.model';
 
 @Component({
   selector: 'app-embodiments',
-  imports: [NgFor, ToggleSwitch, FormsModule, NgClass, LoaderComponent, AnimalComponent],
+  imports: [
+    NgFor,
+    ToggleSwitch,
+    FormsModule,
+    NgClass,
+    LoaderComponent,
+    AnimalComponent,
+    MultiSelectModule,
+  ],
   templateUrl: './embodiments.component.html',
   styleUrl: './embodiments.component.scss',
 })
@@ -18,5 +28,19 @@ export class EmbodimentsComponent extends HomeComponent {
   private readonly stateService = inject(StateService);
   animals = computed(() => this.stateService.animals());
   isTotemShown = false;
-  isDataLoading = computed(() => false);
+  isDataLoading = computed(() => this.stateService.isDataLoading());
+  selectedAnimals = signal<IAnimal[]>([]);
+  filteredAnimals = computed(() => {
+    return this.selectedAnimals().length ? this.selectedAnimals() : this.animals();
+  });
+
+  constructor() {
+    super();
+    this.stateService.addBackendDataToState();
+
+    effect(() => {
+      const selectedAnimals = this.selectedAnimals();
+      console.log('Selected Animals:', selectedAnimals);
+    });
+  }
 }
