9034cca8a26a4b29437114db8bdcaf50be874178
KSkryabin <k.skryabin@mobidev.biz>
2025-08-12 14:08:54 +0300

    refactor(profile): restructure profile component and extract header into separate component

---
+++ src/app/features/profile/profile-header/profile-header.component.ts
@@ -0,0 +1,17 @@
+import { Component, computed, inject } from '@angular/core';
+import { AvatarModule } from 'primeng/avatar';
+import { StateService } from '../../../core/services/state.service';
+
+@Component({
+  selector: 'app-profile-header',
+  imports: [AvatarModule],
+  templateUrl: './profile-header.component.html',
+  styleUrl: './profile-header.component.scss',
+})
+export class ProfileHeaderComponent {
+  private stateService = inject(StateService);
+  photoURL = computed(() => this.stateService.profile()?.avatarUrl || '');
+  displayName = computed(() => this.stateService.displayName());
+  email = computed(() => this.stateService.profile()?.email || '');
+  country = computed(() => this.stateService.profile()?.country || '');
+}
--- src/app/features/profile/profile.component.ts
+++ src/app/features/profile/profile.component.ts
@@ -1,17 +1,16 @@
 import { Component, computed, inject } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
 import { StateService } from '../../core/services/state.service';
-import { AvatarModule } from 'primeng/avatar';
+import { ProfileHeaderComponent } from './profile-header/profile-header.component';
 
 @Component({
   selector: 'app-profile',
-  imports: [TranslateModule, AvatarModule],
+  imports: [TranslateModule, ProfileHeaderComponent],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.scss',
 })
 export class ProfileComponent {
   private stateService = inject(StateService);
-  photoURL = computed(() => this.stateService.profile()?.avatarUrl || '');
   displayName = computed(() => this.stateService.displayName());
   email = computed(() => this.stateService.profile()?.email || '');
   country = computed(() => this.stateService.profile()?.country || '');
