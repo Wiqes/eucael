d32e36b126d0fa9e75e2512f1397b71c8cfc8a0b
KSkryabin <k.skryabin@mobidev.biz>
2025-08-13 15:37:56 +0300

    feat(profile): create ProfileActions component and refactor profile layout

---
+++ src/app/features/profile/profile-actions/profile-actions.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
+
+@Component({
+  selector: 'app-profile-actions',
+  imports: [TranslateModule],
+  templateUrl: './profile-actions.component.html',
+  styleUrl: './profile-actions.component.scss',
+})
+export class ProfileActionsComponent {}
--- src/app/features/profile/profile.component.ts
+++ src/app/features/profile/profile.component.ts
@@ -2,10 +2,11 @@ import { Component, computed, inject } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
 import { StateService } from '../../core/services/state.service';
 import { ProfileHeaderComponent } from './profile-header/profile-header.component';
+import { ProfileActionsComponent } from './profile-actions/profile-actions.component';
 
 @Component({
   selector: 'app-profile',
-  imports: [TranslateModule, ProfileHeaderComponent],
+  imports: [TranslateModule, ProfileHeaderComponent, ProfileActionsComponent],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.scss',
 })
