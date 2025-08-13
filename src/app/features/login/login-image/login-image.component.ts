1562172763e7199c9ad83ea69bfaafe1d59c7d7e
KSkryabin <k.skryabin@mobidev.biz>
2025-08-13 10:19:39 +0300

    refactor(login): extract background image section into separate component for better modularity

---
+++ src/app/features/login/login-image/login-image.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login-image',
+  imports: [],
+  templateUrl: './login-image.component.html',
+  styleUrl: './login-image.component.scss'
+})
+export class LoginImageComponent {
+
+}
