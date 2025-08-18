751a8ce86667778347b7868a1ffc91c2d0856681
KSkryabin <k.skryabin@mobidev.biz>
2025-08-18 19:38:11 +0300

    refactor(app): add loading overlay to main content and enhance imports for state management

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,11 +5,13 @@ import { ToastComponent } from './shared/ui/toast/toast.component';
 import { LanguageService } from './core/services/language.service';
 import { LoginService } from './core/services/auth/login.service';
 import { AuthService } from './core/services/auth/auth.service';
+import { StateService } from './core/services/state.service';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FormsModule, ToastComponent],
+  imports: [RouterOutlet, FormsModule, ToastComponent, NgClass],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
@@ -17,7 +19,9 @@ export class AppComponent implements OnInit {
   private readonly languageService = inject(LanguageService);
   private readonly loginService = inject(LoginService);
   protected readonly authService = inject(AuthService);
+  private readonly stateService = inject(StateService);
   isLoggedIn = computed(() => this.loginService.isLoggedIn());
+  isDataLoading = computed(() => this.stateService.isDataLoading());
 
   ngOnInit(): void {
     const element = document.querySelector('html');
