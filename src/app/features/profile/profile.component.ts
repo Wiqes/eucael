66dd4404161faa3ca999e2023a5f436354b8cb7b
KSkryabin <k.skryabin@mobidev.biz>
2025-08-20 21:54:05 +0300

    feat(profile): enhance registration date display with locale-based formatting

--- src/app/features/profile/profile.component.ts
+++ src/app/features/profile/profile.component.ts
@@ -1,6 +1,7 @@
 import { Component, computed, inject } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
 import { StateService } from '../../core/services/state.service';
+import { LanguageService } from '../../core/services/language.service';
 import { ProfileHeaderComponent } from './profile-header/profile-header.component';
 import { DatePipe, NgIf } from '@angular/common';
 
@@ -12,10 +13,13 @@ import { DatePipe, NgIf } from '@angular/common';
 })
 export class ProfileComponent {
   private stateService = inject(StateService);
+  private languageService = inject(LanguageService);
+
   displayName = computed(() => this.stateService.displayName());
   email = computed(() => this.stateService.profile()?.email || '');
   country = computed(() => this.stateService.profile()?.country || '');
   isDataLoading = computed(() => this.stateService.isDataLoading());
+  locale = computed(() => this.languageService.locale());
   registrationDate = computed(() => {
     const date = new Date(this.stateService.user()?.createdAt || '');
     return isNaN(date.getTime()) ? '' : date;
