453537db9ff28bd8e70af7b36a958bcaf7ddbcfa
KSkryabin <k.skryabin@mobidev.biz>
2025-08-20 22:10:05 +0300

    feat(profile): simplify registration date display with formatted date function

--- src/app/features/profile/profile.component.ts
+++ src/app/features/profile/profile.component.ts
@@ -3,11 +3,11 @@ import { TranslateModule } from '@ngx-translate/core';
 import { StateService } from '../../core/services/state.service';
 import { LanguageService } from '../../core/services/language.service';
 import { ProfileHeaderComponent } from './profile-header/profile-header.component';
-import { DatePipe, NgIf } from '@angular/common';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-profile',
-  imports: [TranslateModule, ProfileHeaderComponent, NgIf, DatePipe],
+  imports: [TranslateModule, ProfileHeaderComponent, NgIf],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.scss',
 })
@@ -23,5 +23,17 @@ export class ProfileComponent {
   registrationDate = computed(() => {
     const date = new Date(this.stateService.user()?.createdAt || '');
     return isNaN(date.getTime()) ? '' : date;
+  });
+  formattedRegistrationDate = computed(() => {
+    const date = this.registrationDate();
+    const locale = this.locale();
+    if (!date) return '';
+    return new Intl.DateTimeFormat(locale, {
+      year: 'numeric',
+      month: 'long',
+      day: 'numeric',
+      hour: '2-digit',
+      minute: '2-digit',
+    }).format(date);
   });
 }
