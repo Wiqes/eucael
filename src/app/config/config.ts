47402694bf8d23179b37350ac56bb574bf1dc6b7
KSkryabin <k.skryabin@mobidev.biz>
2025-08-20 21:50:01 +0300

    feat(date): add localized date pipe to format dates based on user locale

--- src/app/config/config.ts
+++ src/app/config/config.ts
@@ -5,7 +5,53 @@ import { translationProvider } from './translation.provider';
 import { primeNGProvider } from './prime-ng.provider';
 import { routerProvider } from './router.provider';
 import { authInterceptor } from '../core/auth.interceptor';
-import { MessageService } from 'primeng/api';
+
+import localeEnUS from '@angular/common/locales/en';
+import localeEsES from '@angular/common/locales/es';
+import localeZhCN from '@angular/common/locales/zh';
+import localeHiIN from '@angular/common/locales/hi';
+import localeArSA from '@angular/common/locales/ar-SA';
+import localeBnBD from '@angular/common/locales/bn';
+import localePtBR from '@angular/common/locales/pt';
+import localeRuRU from '@angular/common/locales/ru';
+import localeJaJP from '@angular/common/locales/ja';
+import localePaIN from '@angular/common/locales/pa';
+import localeDeDE from '@angular/common/locales/de';
+import localeFrFR from '@angular/common/locales/fr';
+import localeTrTR from '@angular/common/locales/tr';
+import localeItIT from '@angular/common/locales/it';
+import localeViVN from '@angular/common/locales/vi';
+import localeTaIN from '@angular/common/locales/ta';
+import localeUrPK from '@angular/common/locales/ur';
+import localeIdID from '@angular/common/locales/id';
+import localeSwKE from '@angular/common/locales/sw';
+import localeMrIN from '@angular/common/locales/mr';
+import localeTeIN from '@angular/common/locales/te';
+import localeUkUA from '@angular/common/locales/uk';
+import { registerLocaleData } from '@angular/common';
+
+registerLocaleData(localeEnUS, 'en-US');
+registerLocaleData(localeEsES, 'es-ES');
+registerLocaleData(localeZhCN, 'zh-CN');
+registerLocaleData(localeHiIN, 'hi-IN');
+registerLocaleData(localeArSA, 'ar-SA');
+registerLocaleData(localeBnBD, 'bn-BD');
+registerLocaleData(localePtBR, 'pt-BR');
+registerLocaleData(localeRuRU, 'ru-RU');
+registerLocaleData(localeJaJP, 'ja-JP');
+registerLocaleData(localePaIN, 'pa-IN');
+registerLocaleData(localeDeDE, 'de-DE');
+registerLocaleData(localeFrFR, 'fr-FR');
+registerLocaleData(localeTrTR, 'tr-TR');
+registerLocaleData(localeItIT, 'it-IT');
+registerLocaleData(localeViVN, 'vi-VN');
+registerLocaleData(localeTaIN, 'ta-IN');
+registerLocaleData(localeUrPK, 'ur-PK');
+registerLocaleData(localeIdID, 'id-ID');
+registerLocaleData(localeSwKE, 'sw-KE');
+registerLocaleData(localeMrIN, 'mr-IN');
+registerLocaleData(localeTeIN, 'te-IN');
+registerLocaleData(localeUkUA, 'uk-UA');
 
 export const appConfig: ApplicationConfig = {
   providers: [
