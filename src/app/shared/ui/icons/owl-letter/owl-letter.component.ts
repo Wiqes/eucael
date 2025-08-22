b1b6cebdc60b6fffe86e961bcc46a0a21dee8aff
KSkryabin <k.skryabin@mobidev.biz>
2025-08-22 22:23:00 +0300

    feat(owl-letter): add owl letter icon component with SVG and styles

---
+++ src/app/shared/ui/icons/owl-letter/owl-letter.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-owl-letter',
+  imports: [],
+  templateUrl: './owl-letter.component.html',
+  styleUrl: './owl-letter.component.scss'
+})
+export class OwlLetterComponent {
+
+}
