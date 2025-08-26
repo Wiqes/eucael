aa5aad7620f135f2c237e4e35e0bca1cbb878bfc
KSkryabin <k.skryabin@mobidev.biz>
2025-08-26 20:24:59 +0300

    feat(profile): add InfoItem component with template and styles

---
+++ src/app/features/profile/info-item/info-item.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-info-item',
+  imports: [],
+  templateUrl: './info-item.component.html',
+  styleUrl: './info-item.component.scss'
+})
+export class InfoItemComponent {
+
+}
