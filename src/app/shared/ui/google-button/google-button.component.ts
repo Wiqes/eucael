447e0af6aea92319e1fe01d9945b01f687084388
KSkryabin <k.skryabin@mobidev.biz>
2025-08-29 19:25:35 +0300

    feat(google-button): inject FingerprintService and update googleLogin method to include fingerprint

--- src/app/shared/ui/google-button/google-button.component.ts
+++ src/app/shared/ui/google-button/google-button.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
 import { GoogleIconComponent } from '../google-icon/google-icon.component';
 import { environment } from '../../../../environments/environment';
 import { TranslateModule } from '@ngx-translate/core';
+import { FingerprintService } from '../../../core/services/fingerprint.service';
 
 @Component({
   selector: 'app-google-button',
@@ -11,7 +12,15 @@ import { TranslateModule } from '@ngx-translate/core';
   styleUrls: ['./google-button.component.scss'],
 })
 export class GoogleButtonComponent {
+  private readonly fingerprintService = inject(FingerprintService);
+
   googleLogin() {
-    window.location.href = `${environment.API_URL}/auth/google`;
+    window.location.href = `${
+      environment.API_URL
+    }/auth/google?fingerprint=${this.getFingerprint()}`;
+  }
+
+  private getFingerprint(): string {
+    return this.fingerprintService.getFingerprint();
   }
 }
