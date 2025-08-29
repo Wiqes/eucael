e8594ece8585918e9848ef9e866b453b7a6ca825
KSkryabin <k.skryabin@mobidev.biz>
2025-08-29 19:18:04 +0300

    refactor(config): remove unused import from HttpClient in config.ts

--- src/app/config/config.ts
+++ src/app/config/config.ts
@@ -1,5 +1,5 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideHttpClient, withInterceptors, withInterceptorsFromDi } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { translationProvider } from './translation.provider';
 import { primeNGProvider } from './prime-ng.provider';
