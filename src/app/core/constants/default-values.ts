6ebe461aec89878bad11bc3ee1b5e56115bc2dde
KSkryabin <k.skryabin@mobidev.biz>
2025-09-10 16:21:32 +0300

    refactor(default-values): update default avatar URL to the latest image

--- src/app/core/constants/default-values.ts
+++ src/app/core/constants/default-values.ts
@@ -1,2 +1,2 @@
 export const DEFAULT_AVATAR_URL =
-  'https://wiqes-images.s3.us-east-1.amazonaws.com/0f91ddcd-5de7-43e2-b919-0d1b8f1dfd26-avatar.png';
+  'https://wiqes-images.s3.us-east-1.amazonaws.com/0dc7669a-a358-4410-8b3a-3f4510ef0cc6-avatar.png';
