2dd57f2d772d5c013d7b1900e99173ca7ec518fe
KSkryabin <k.skryabin@mobidev.biz>
2025-09-13 14:53:44 +0300

    refactor: update domain references from celestiel.org to celestiel.net across environment, sitemap, and robots files

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  DOMAIN: 'https://celestiel.org',
-  API_URL: 'https://api.celestiel.org',
-  WS_URL: 'wss://api.celestiel.org',
+  DOMAIN: 'https://celestiel.net',
+  API_URL: 'https://api.celestiel.net',
+  WS_URL: 'wss://api.celestiel.net',
 };
