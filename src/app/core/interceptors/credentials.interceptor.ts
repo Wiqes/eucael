5206b2ab7089540d203fb824e18ab43b837b3c7a
KSkryabin <k.skryabin@mobidev.biz>
2025-09-26 12:39:48 +0300

    refactor(credentials.interceptor): skip credentials for S3 requests in interceptor

--- src/app/core/interceptors/credentials.interceptor.ts
+++ src/app/core/interceptors/credentials.interceptor.ts
@@ -1,6 +1,12 @@
 import { HttpInterceptorFn } from '@angular/common/http';
 
 export const withCredentialsInterceptor: HttpInterceptorFn = (req, next) => {
+  // Skip credentials for S3 requests
+  const s3Host = 'https://wiqes-images.s3.us-east-1.amazonaws.com';
+  if (req.url.includes(s3Host)) {
+    return next(req);
+  }
+
   const credentialsReq = req.clone({
     withCredentials: true,
   });
