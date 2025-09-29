49985642c665364a37d6ea8b947c4888e0d981c4
KSkryabin <k.skryabin@mobidev.biz>
2025-09-29 23:06:14 +0300

    refactor(loader): remove unused imports and lifecycle methods

--- src/app/shared/ui/loader/loader.component.ts
+++ src/app/shared/ui/loader/loader.component.ts
@@ -1,8 +1,5 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { FantasyLoaderComponent } from './fantasy-loader/fantasy-loader.component';
-import { AuthTokenStateService } from '../../../core/services/state/auth-token-state.service';
-import { StateService } from '../../../core/services/state/state.service';
-import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-loader',
@@ -10,16 +7,4 @@ import { Router } from '@angular/router';
   templateUrl: './loader.component.html',
   styleUrl: './loader.component.scss',
 })
-export class LoaderComponent implements OnInit {
-  private readonly authTokenStateService = inject(AuthTokenStateService);
-  private readonly stateService = inject(StateService);
-  private readonly router = inject(Router);
-
-  ngOnInit(): void {
-    const currentRoute = this.router.url;
-    if (currentRoute === '/') {
-      this.authTokenStateService.isRefreshing.set(false);
-      this.stateService.isDataLoading.set(false);
-    }
-  }
-}
+export class LoaderComponent {}
