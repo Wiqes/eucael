13f7b2f201d34791c65934f23092e581514925e6
KSkryabin <k.skryabin@mobidev.biz>
2025-09-29 23:34:27 +0300

    refactor(loader): simplify loader component by removing unused imports and visibility condition

--- src/app/shared/ui/loader/loader.component.ts
+++ src/app/shared/ui/loader/loader.component.ts
@@ -1,15 +1,10 @@
-import { Component, computed, inject } from '@angular/core';
+import { Component } from '@angular/core';
 import { FantasyLoaderComponent } from './fantasy-loader/fantasy-loader.component';
-import { Router } from '@angular/router';
-import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-loader',
-  imports: [FantasyLoaderComponent, NgIf],
+  imports: [FantasyLoaderComponent],
   templateUrl: './loader.component.html',
   styleUrl: './loader.component.scss',
 })
-export class LoaderComponent {
-  private readonly router = inject(Router);
-  isVisiable = computed(() => this.router.url !== '/');
-}
+export class LoaderComponent {}
