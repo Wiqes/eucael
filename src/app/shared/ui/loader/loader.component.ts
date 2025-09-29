9555e22acc6c441a2dbc1016de5977ba4531bab6
KSkryabin <k.skryabin@mobidev.biz>
2025-09-29 23:33:44 +0300

    fix(loader): conditionally render loader based on visibility state

--- src/app/shared/ui/loader/loader.component.ts
+++ src/app/shared/ui/loader/loader.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject } from '@angular/core';
 import { FantasyLoaderComponent } from './fantasy-loader/fantasy-loader.component';
+import { Router } from '@angular/router';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-loader',
-  imports: [FantasyLoaderComponent],
+  imports: [FantasyLoaderComponent, NgIf],
   templateUrl: './loader.component.html',
   styleUrl: './loader.component.scss',
 })
-export class LoaderComponent {}
+export class LoaderComponent {
+  private readonly router = inject(Router);
+  isVisiable = computed(() => this.router.url !== '/');
+}
