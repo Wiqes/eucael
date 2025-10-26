9378f6ed0fde97c8e21e580a33ec30e932676dcd
KSkryabin <k.skryabin@mobidev.biz>
2025-10-26 15:13:06 +0200

    feat: enhance owl-icon component with dark mode support and update related templates

--- src/app/shared/ui/icons/owl-icon/owl-icon.component.ts
+++ src/app/shared/ui/icons/owl-icon/owl-icon.component.ts
@@ -1,9 +1,12 @@
-import { Component } from '@angular/core';
+import { NgIf } from '@angular/common';
+import { Component, input } from '@angular/core';
 
 @Component({
   selector: 'app-owl-icon',
-  imports: [],
+  imports: [NgIf],
   templateUrl: './owl-icon.component.html',
   styleUrl: './owl-icon.component.scss',
 })
-export class OwlIconComponent {}
+export class OwlIconComponent {
+  dark = input<boolean>(false);
+}
