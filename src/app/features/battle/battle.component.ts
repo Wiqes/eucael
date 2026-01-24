e4440cda390fe0325fec3575b8c56c5b98ccb962
KSkryabin <k.skryabin@mobidev.biz>
2026-01-24 12:51:17 +0200

    fix(battle): update startBattle method to include all characters for battle initiation

--- src/app/features/battle/battle.component.ts
+++ src/app/features/battle/battle.component.ts
@@ -51,7 +51,22 @@ export class BattleComponent implements OnInit, OnDestroy {
   }
 
   startBattle(): void {
-    this.battleService.startBattle([BATTLE_CHARACTERS['RAT']], [BATTLE_CHARACTERS['BEAR']]);
+    this.battleService.startBattle(
+      [
+        BATTLE_CHARACTERS['RAT'],
+        BATTLE_CHARACTERS['CAT'],
+        BATTLE_CHARACTERS['GIRAFFE'],
+        BATTLE_CHARACTERS['HORSE'],
+        BATTLE_CHARACTERS['BEAR'],
+      ],
+      [
+        BATTLE_CHARACTERS['BEAR'],
+        BATTLE_CHARACTERS['HORSE'],
+        BATTLE_CHARACTERS['GIRAFFE'],
+        BATTLE_CHARACTERS['CAT'],
+        BATTLE_CHARACTERS['RAT'],
+      ],
+    );
   }
 
   resetAndTerminateBattle(): void {
