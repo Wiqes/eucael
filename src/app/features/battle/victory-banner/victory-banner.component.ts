76ea4929c74be213fb5cda87d903ab22c05b598f
KSkryabin <k.skryabin@mobidev.biz>
2026-03-02 21:05:47 +0200

    feat(battle): implement terminate battle functionality in victory banner and update button styles

--- src/app/features/battle/victory-banner/victory-banner.component.ts
+++ src/app/features/battle/victory-banner/victory-banner.component.ts
@@ -1,14 +1,20 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ButtonModule } from 'primeng/button';
 import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-victory-banner',
   standalone: true,
-  imports: [CommonModule, TranslateModule],
+  imports: [CommonModule, ButtonModule, TranslateModule],
   templateUrl: './victory-banner.component.html',
   styleUrls: ['./victory-banner.component.scss'],
 })
 export class VictoryBannerComponent {
   @Input({ required: true }) winner!: string;
+  @Output() readonly terminateBattle = new EventEmitter<void>();
+
+  onTerminateBattle(): void {
+    this.terminateBattle.emit();
+  }
 }
